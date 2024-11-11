6ab21d942166fb81fa26d26165606d145ce5e639
kaaarolin <karolin_57@hotmail.com>
2024-11-11 20:06:43 +0100

    k

---
+++ Dice game/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+            Random random = new Random();
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a: " + playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("The enemy AI rolled a: " + enemyRandomNum);
+                Console.WriteLine();
+
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player wins this round");
+                }
+                else if (enemyRandomNum > playerRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy AI wins this round");
+                }
+                else
+                {
+                    Console.WriteLine("Draw!");
+                }
+
+                Console.WriteLine("The score is now - Player: " + playerPoints + " Enemy: " + enemyPoints);
+                Console.WriteLine();
+            }
+
+            if (playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You won!");
+            }
+            else if (playerPoints < enemyPoints)
+            {
+                Console.WriteLine("You lost :/");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+            }
+
+                    
+            Console.ReadKey();
+        }
+    }
+}
